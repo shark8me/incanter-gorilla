15855090de7177d3a57b7d38b7a7986cbd340b97
kirankarkera <kiran.karkera@bridgei2i.com>
2015-08-28 08:42:47 +0530

    view dataset as a table in the repl

--- src/incanter_gorilla/render.clj
+++ src/incanter_gorilla/render.clj
@@ -1,5 +1,6 @@
 (ns incanter-gorilla.render
   (:require [clojure.data.codec.base64 :as b64]
+            [incanter.core :as incore]
             [gorilla-renderable.core :as render]))
 
 (defn chart->byte-array
@@ -23,3 +24,26 @@
       {:type    :html
        :content (format "<img src=\"data:image/PNG;base64,%1$s\"/>" (String. (b64/encode bytes)))
        :value   (pr-str self)})))
+
+(defn- list-like
+  "util function used in render"
+  [data value open close separator]
+  {:type :list-like
+   :open open
+   :close close
+   :separator separator
+   :items data
+   :value value})
+
+
+ ;extend dataset type to show in gorilla-repl interface
+(extend-type incanter.core.Dataset
+    render/Renderable
+    (render [self]
+            (let [rendfn (fn [open close sep r] (list-like (map render/render r) (pr-str r) open close sep))
+                  rows (map (partial rendfn "<tr><td>" "</td></tr>" "</td><td>") (incore/to-list self))
+                  heading (if-let [cols (:column-names self)]
+                            [(rendfn "<tr><th>" "</th></tr>" "</th><th>" cols)]
+                            [])
+                  body (list-like (concat heading rows) (pr-str self) "<center><table>" "</table></center>" "\n")]
+              body)))
